68329c51a0dd81aa85869a0300a15a2c1873f035
rouxxi <laskar.gwenvael@gmail.com>
2020-11-19 16:45:07 +0100

    little advence on the long road

--- components/InventoryMySelf.jsx
+++ components/InventoryMySelf.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Inventory = styled.div`
+
+`;
 
 
 class InventoryMySelf extends React.Component{
+    constructor(props){
+        super(props);
+    }
     render(){
         return(
-            <div>
-                
-            </div>
+            <Inventory>
+                <img src={this.props.image} alt={this.props.name}/>
+            </Inventory>
         )
     }
 }
--- components/InventoryTavern.jsx
+++ components/InventoryTavern.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Inventory = styled.div`
+
+`;
 
 
 class InventoryTavern extends React.Component{
+    constructor(props){
+        super(props);
+    }
     render(){
         return(
-            <div>
-
-            </div>
+            <Inventory>
+                <img src={this.props.image} alt={this.props.name}/>
+            </Inventory>
         )
     }
 }
