2d59ce64b3c42df11c244c9297fbecb0b84eac07
fdebrabant <debrabant.fanny@gmail.com>
2020-11-19 17:42:52 +0100

    add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import "./App.css";
+import styled from 'styled-components';
+import Backgroundimg from './background.svg';
+
+const Apps = styled.div`
+
+    margin: 0;
+    padding: 0;
+    background-image: url(${Backgroundimg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100vw;
+    height: 150vh;
+    background-position: fixed;
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Epic Recipe</h1>
-    </div>
+    <Apps>
+      
+    </Apps>
   );
 }
 
