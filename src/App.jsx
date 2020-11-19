5a2d7d428d8946f3c83557aded3613fa6cbc17df
thomas <thomas.chalanson@sfr.fr>
2020-11-19 18:04:20 +0100

    fix the css Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import styled from 'styled-components';
-import Backgroundimg from './background.svg';
+import styled from "styled-components";
+import Backgroundimg from "./background.svg";
 
 const Apps = styled.div`
-
-    margin: 0;
-    padding: 0;
-    background-image: url(${Backgroundimg});
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100vw;
-    height: 150vh;
-    background-position: fixed;
-
+  margin: 0;
+  padding: 0;
+  background-image: url(${Backgroundimg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  padding-bottom: 20em;
 `;
 
 function App() {
   return (
-    <Apps>
-      
-    </Apps>
+    <div className="App">
+      <Apps></Apps>
+    </div>
   );
 }
 
